236e6eff81dabe161ff298ae7a845d10d6375674
Michael Baaske <mikewithak@gmail.com>
2017-10-10 12:00:12 -0700

    add logout and check session for dashboard page

--- public/dash.php
+++ public/dash.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+//check if they're logged in, if not send them back to login page.
+if(!isset($_SESSION['id'])) {
+    header("Location: login.php");
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <?php include 'partials/header.php' ?>
@@ -6,7 +15,7 @@
 <div class="mx-auto">
     <div class="card text-center">
         <div class="card-header">
-            Log In
+            <a href="logout.php">Logout</a>
         </div>
         <div class="card-body">
             <form>
