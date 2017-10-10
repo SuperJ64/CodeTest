a83205b8acec86d7248cb484dc8b7d31ee46517d
Michael Baaske <mikewithak@gmail.com>
2017-10-10 10:11:00 -0700

    add login page and dashboard, fill in later

---
+++ public/dash.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'partials/header.php' ?>
+
+<body>
+<div class="mx-auto">
+    <div class="card text-center">
+        <div class="card-header">
+            Log In
+        </div>
+        <div class="card-body">
+            <form>
+
+            </form>
+        </div>
+        <div class="card-footer text-muted">
+            Or Register new account
+        </div>
+    </div>
+</div>
+
+<?php include 'partials/footer.php' ?>
+
+</body>
+</html>
